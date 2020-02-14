c9c58341619da2d0a710921a9344f5b9fc20d85c
Antonio Baldi <a.baldi20@studenti.unisa.it>
2020-02-14 11:00:06 +0100

    Added function for have divverent notation

--- Bin Finder/ViewController.swift
+++ Bin Finder/ViewController.swift
@@ -93,51 +93,70 @@ class ViewController: UIViewController, CLLocationManagerDelegate, MKMapViewDele
     }
     
     func mapView(_ mapView: MKMapView,
-                  didSelect view: MKAnnotationView) {
-         // Tells the delegate that one of its annotation views was selected.
-     }
-     
-    func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
-         if (annotation is MKUserLocation) {
-             return nil
-         }
-         
-         let reuseId = "carta"
-         
-         var anView = mapView.dequeueReusableAnnotationView(withIdentifier: reuseId)
-         if anView == nil {
-             anView = MKAnnotationView(annotation: annotation, reuseIdentifier: reuseId)
-             anView?.image = UIImage(named:"prova")
-             anView?.canShowCallout = true
-         }
-         else {
-             //we are re-using a view, update its annotation reference...
-             anView?.annotation = annotation
-         }
-         
-         return anView
-     }
-    
-    struct Bin {
-      var type: String
-      var lattitude: CLLocationDegrees
-      var longtitude: CLLocationDegrees
+                 didSelect view: MKAnnotationView) {
+        // Tells the delegate that one of its annotation views was selected.
     }
     
-    let bins = [Bin(type: "Emirates Stadium", lattitude: 40.7723, longtitude: 14.7899),
-                    Bin(type: "Stamford Bridge", lattitude: 51.4816, longtitude: -0.191034),
-                    Bin(type: "White Hart Lane", lattitude: 51.6033, longtitude: -0.065684),
-                    Bin(type: "Olympic Stadium", lattitude: 51.5383, longtitude: -0.016587),
-                    Bin(type: "Old Trafford", lattitude: 53.4631, longtitude: -2.29139),
-                    Bin(type: "Anfield", lattitude: 53.4308, longtitude: -2.96096)]
+    func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
+        if (annotation is MKUserLocation) {
+            return nil
+        }
+        
+        let reuseId = "carta"
+        
+        var anView = mapView.dequeueReusableAnnotationView(withIdentifier: reuseId)
+        if anView == nil {
+            anView = MKAnnotationView(annotation: annotation, reuseIdentifier: reuseId)
+            print("Nome della notazione \(annotation.title) ")
+            switch annotation.title {
+            case "Paper":
+                anView?.image = UIImage(named:"prova")
+                anView?.canShowCallout = true
+            case "Glass":
+                anView?.image = UIImage(named:"img1")
+                anView?.canShowCallout = true
+            case "Plastic & Metals":
+                anView?.image = UIImage(named:"img2")
+                anView?.canShowCallout = true
+            case "Mixed waste":
+                anView?.image = UIImage(named:"prova")
+                anView?.canShowCallout = true
+            case "Organic waste":
+                anView?.image = UIImage(named:"prova")
+                anView?.canShowCallout = true
+            default:
+                print("err")
+            }
+            
+        }
+        else {
+            //we are re-using a view, update its annotation reference...
+            anView?.annotation = annotation
+        }
+        
+        return anView
+    }
     
-
+    struct Bin {
+        var type: String
+        var lattitude: CLLocationDegrees
+        var longtitude: CLLocationDegrees
+    }
+    
+    let bins = [Bin(type: "Paper", lattitude: 40.7723, longtitude: 14.7899),
+                Bin(type: "Glass", lattitude: 51.4816, longtitude: -0.191034),
+                Bin(type: "Plastic & Metals", lattitude: 51.6033, longtitude: -0.065684),
+                Bin(type: "Mixed waste", lattitude: 51.5383, longtitude: -0.016587),
+                Bin(type: "Organic waste", lattitude: 53.4631, longtitude: -2.29139),
+                Bin(type: "Anfield", lattitude: 53.4308, longtitude: -2.96096)]
+    
+    
     func fetchBinsOnMap(_ bins: [Bin]) {
         for bin in bins {
-        let annotations = MKPointAnnotation()
-          annotations.title = bin.type
-          annotations.coordinate = CLLocationCoordinate2D(latitude: bin.lattitude, longitude: bin.longtitude)
-          mapView.addAnnotation(annotations)
+            let annotations = MKPointAnnotation()
+            annotations.title = bin.type
+            annotations.coordinate = CLLocationCoordinate2D(latitude: bin.lattitude, longitude: bin.longtitude)
+            mapView.addAnnotation(annotations)
         }
     }
 }
