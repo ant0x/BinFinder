4674219dfb3d097c7d048b933afbedf5438befac
ciromaione <ciroma809@gmail.com>
2020-02-14 12:12:09 +0100

    aggiunta classe bin

---
+++ Bin Finder/Bin.swift
@@ -0,0 +1,13 @@
+//
+//  Bin.swift
+//  Bin Finder
+//
+//  Created by Ciro Maione on 14/02/2020.
+//  Copyright © 2020 Antonio Baldi. All rights reserved.
+//
+
+import UIKit
+
+class Bin: NSObject {
+
+}
